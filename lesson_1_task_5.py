9b18ca16e82f2afeff1a09a3618a8a8bde012105
tihoola78-cmyk <tihoola78@gmail.com>
2026-04-27 09:23:03 +0300

    Add files via upload

---
+++ lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_digit(num):
+    print(num, end="")
+
+print_digit(8)
+print_digit(8)
+print_digit(0)
+print_digit(0)
+print_digit(5)
+print_digit(5)
+print_digit(5)
+print_digit(3)
+print_digit(5)
+print_digit(3)
+print_digit(5)
+print()
