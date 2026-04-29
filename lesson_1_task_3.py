f1bbdf42150e33e1a32d489da03628834b37908a
tihoola78-cmyk <tihoola78@gmail.com>
2026-04-29 08:41:15 +0300

    Update lesson_1_task_3.py

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Ольга:")
-last_name = input("Тихомирова:")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 print("Вас зовут:", last_name, first_name)
