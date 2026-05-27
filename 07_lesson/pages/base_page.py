86eac75db69d9440a0bb537e4a5daadaed8f9549
Honor <tihoola78@gmail.com>
2026-05-27 12:00:43 +0300

    Урок 7: Page Object Model (калькулятор + магазин) - тесты проходят

--- 07_lesson/pages/base_page.py
+++ 07_lesson/pages/base_page.py
@@ -5,14 +5,13 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 60)
 
     def find_element(self, locator):
         return self.wait.until(EC.presence_of_element_located(locator))
 
     def click(self, locator):
-        element = self.find_element(locator)
-        element.click()
+        self.find_element(locator).click()
 
     def input_text(self, locator, text):
         element = self.find_element(locator)
