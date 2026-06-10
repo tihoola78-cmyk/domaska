27efd6f2cce3c578730d7654bbe7162f9579b36e
Honor <tihoola78@gmail.com>
2026-06-10 21:23:21 +0300

    Урок 10: Allure-декораторы в тестах PageObject

---
+++ lesson_10/tests/conftest.py
@@ -0,0 +1,22 @@
+﻿import sys
+from pathlib import Path
+
+# Добавляем папку lesson_10 в sys.path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import allure
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def chrome_driver():
+    options = Options()
+    options.add_argument('--ignore-certificate-errors')
+    with allure.step("Запуск Chrome браузера"):
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+    yield driver
+    with allure.step("Закрытие браузера"):
+        driver.quit()
